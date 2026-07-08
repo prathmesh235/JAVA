5780061a618e08223e4805c0cbd592cd6784c6f3
Prathmesh Panmand <prathmeshpanmand23@gmail.com>
2026-07-08 15:54:10 +0530

    Defining a array by 2nd method

---
+++ def2_array.java
@@ -0,0 +1,19 @@
+
+//Defining a array by method 2
+public class def2_array {
+    public static void main(String[] args) {
+        //type[] array.name=new type[size]
+
+        int marks[]={90,70,99};
+            // marks[0]=90;
+            // marks[1]=70;
+            // marks[2]=99;
+            
+            //System.out.println(marks[0]);
+            //System.out.println(marks[1]);
+            //System.out.println(marks[2]);
+            for( int i=0;i<3;i++){
+                System.out.println(marks[i]);
+            }
+    }
+}

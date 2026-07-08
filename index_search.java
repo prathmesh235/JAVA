bc03f7139fe60092ef2512afa7efad557148254e
Prathmesh Panmand <prathmeshpanmand23@gmail.com>
2026-07-08 16:44:03 +0530

    Add input from user and find the index

---
+++ index_search.java
@@ -0,0 +1,21 @@
+// searching index from array
+import java.util.Scanner;
+
+public class index_search {
+    public static void main(String[] args) {
+       Scanner sc = new Scanner(System.in);
+       
+       int size = sc.nextInt();
+       int number[] = new int[size];
+       //input
+       for(int i=0;i<size;i++){
+        number[i]= sc.nextInt();
+       }
+       int x = sc.nextInt();
+       //output
+       for(int i=0;i<number.length;i++){
+        if(number[i]==x)
+        System.out.println("x is found at index:"+i);
+       }
+    }
+}

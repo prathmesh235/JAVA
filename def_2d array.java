ec2045190c7bc24b86ac6b1acf972a1e5dc7a206
Prathmesh Panmand <prathmeshpanmand23@gmail.com>
2026-07-09 18:35:28 +0530

    Defining a 2D array

---
+++ def_2d array.java
@@ -0,0 +1,7 @@
+//Defining a 2D Array
+public class def_2d array {
+    public static void main(String[] args) {
+        //type  array name [][]=new type[row][column];
+        int number[][]=new int[3][5];
+    }
+}

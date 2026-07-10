dded5ec0c22ff9ee3126b97778b4558cd82bdcc1
Prathmesh Panmand <prathmeshpanmand23@gmail.com>
2026-07-10 12:20:10 +0530

    String Declaration

---
+++ def_string.java
@@ -0,0 +1,8 @@
+public class def_string {
+    public static void main(String[] args) {
+        //String Declaration
+        String firstname = "Ram";
+        String  lastname = "Shine";
+        String Sentence =" Hello i'm Prathmesh";
+    }
+}

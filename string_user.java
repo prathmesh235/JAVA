9813fc2143367b1127460bf8618bf42a88b39fcf
Prathmesh Panmand <prathmeshpanmand23@gmail.com>
2026-07-10 12:32:15 +0530

    User String

---
+++ string_user.java
@@ -0,0 +1,9 @@
+import java.util.Scanner;
+
+public class string_user {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String Name = sc.nextLine();
+        System.out.println("Your Name is:"+Name);
+    }
+}

9c311c9645d7c61e56d46fa90cd2922e188ec86b
Prathmesh Panmand <prathmeshpanmand23@gmail.com>
2026-07-10 12:45:44 +0530

    String Length in number count

---
+++ String_length.java
@@ -0,0 +1,9 @@
+public class String_length {
+    public static void main(String[] args) {
+        String FirstName = "Ram";
+        String LastName = "Shinde";
+        String FullName = FirstName +" "+LastName ;
+        //Ram Shinde
+        System.out.println(FullName.length());
+    }
+}

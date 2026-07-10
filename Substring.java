1fa9f1ed66ea247c840da387caeda17ce0c8b362
Prathmesh Panmand <prathmeshpanmand23@gmail.com>
2026-07-10 13:35:41 +0530

    Substring creating on a number

---
+++ Substring.java
@@ -0,0 +1,11 @@
+// import java.util.*;
+
+public class Substring {
+    public static void main(String[] args) {
+
+        String sentence ="PrathmeshPanmand";
+        String name = sentence.substring(9);
+
+        System.out.println(name);
+    }
+}

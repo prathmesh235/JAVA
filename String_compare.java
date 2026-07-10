f5827aafcfca5185c812aa023f433fca393f0520
Prathmesh Panmand <prathmeshpanmand23@gmail.com>
2026-07-10 13:11:06 +0530

    String Compare

---
+++ String_compare.java
@@ -0,0 +1,12 @@
+public class String_compare {
+    public static void main(String[] args) {
+        String name1="Prathm";
+        String name2="Ram";
+
+        if(name1.compareTo(name2)==0){
+            System.out.println("String is equal");
+        }else{
+            System.out.println("String is not equal");
+        }
+    }
+}

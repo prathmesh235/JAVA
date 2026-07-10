ab780905aa2072358ae2471fb2351fd49f7c8ab3
Prathmesh Panmand <prathmeshpanmand23@gmail.com>
2026-07-10 12:40:40 +0530

    Concatenation String

---
+++ String_concatenation.java
@@ -0,0 +1,8 @@
+public class String_concatenation {
+    public static void main(String[] args) {
+        String FirstName = "Ram";
+        String LastName = "Shinde";
+        String FullName = FirstName +" "+LastName ;
+        System.out.println(FullName);
+    }
+}

d9dbd13db6c312d77f14c2b12197643bfccafff6
Prathmesh Panmand <prathmeshpanmand23@gmail.com>
2026-07-11 15:45:12 +0530

    Defining a StringBuilder

---
+++ Def_SB.java
@@ -0,0 +1,8 @@
+// Defining A StringBulder
+public class Def_SB {
+    public static void main (String args[]){
+        StringBuilder sb = new StringBuilder("Ram");
+            System.out.println(sb);
+        
+    }
+}

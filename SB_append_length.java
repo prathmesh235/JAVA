4f9f5139a1eefb61f43b72764c2abd428761e1a3
Prathmesh Panmand <prathmeshpanmand23@gmail.com>
2026-07-11 16:31:34 +0530

    Calculate the length of SB

---
+++ SB_append_length.java
@@ -0,0 +1,11 @@
+
+public class SB_append_length {
+    public static void main(String[] args) {
+        StringBuilder sb=new StringBuilder("H");
+        sb.append("e");
+        sb.append("l");
+        sb.append("l");
+        sb.append("o");
+        System.out.println(sb.length());
+    }
+}

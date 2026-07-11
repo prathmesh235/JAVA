113110439cfbdd8158b3dff219b7ffe703fb6699
Prathmesh Panmand <prathmeshpanmand23@gmail.com>
2026-07-11 16:59:50 +0530

    SB Reverse

---
+++ SB_reverse.java
@@ -0,0 +1,17 @@
+public class SB_reverse {
+    public static void main(String[] args) {
+        StringBuilder sb = new StringBuilder("Prathmesh");
+        
+        for (int i = 0; i < sb.length()/2; i++) {
+            int front =i;
+            int back = sb.length()-1-i; //5-1-0
+
+            char frontchar=sb.charAt(front);
+            char backchar=sb.charAt(back);
+
+            sb.setCharAt(front, backchar);
+            sb.setCharAt(back, frontchar);
+        }
+        System.out.println(sb);
+    }
+}

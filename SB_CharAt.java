823bea7b7d5bce537c50bace3b2cad6893a40584
Prathmesh Panmand <prathmeshpanmand23@gmail.com>
2026-07-11 15:52:04 +0530

    Using CharAt function to finding index number

---
+++ SB_CharAt.java
@@ -0,0 +1,11 @@
+
+//  StringBulder Char At index 0
+public class SB_CharAt {
+    public static void main (String args[]){
+        StringBuilder sb = new StringBuilder("Ram");
+            System.out.println(sb);
+        
+            //charAt index 0
+            System.out.println(sb.charAt(1));
+    }
+}

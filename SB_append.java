f6b5f08fc0d5d9955808823e77c57ccf697e7140
Prathmesh Panmand <prathmeshpanmand23@gmail.com>
2026-07-11 16:28:06 +0530

    SB Append

---
+++ SB_append.java
@@ -0,0 +1,10 @@
+public class SB_append {
+    public static void main(String[] args) {
+        StringBuilder sb=new StringBuilder("H");
+        sb.append("e");
+        sb.append("l");
+        sb.append("l");
+        sb.append("o");
+        System.out.println(sb);
+    }
+}

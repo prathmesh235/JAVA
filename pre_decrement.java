ac3ae6049dcf92da201c5ddbb64a7e967e7a9ecd
Prathmesh Panmand <prathmeshpanmand23@gmail.com>
2026-07-12 15:40:44 +0530

    Pre decrement

---
+++ pre_decrement.java
@@ -0,0 +1,10 @@
+public class pre_decrement {
+    public static void main(String[] args) {
+        int a=10;
+        int b=0;
+
+        b=--a;// a=9 b=9
+        System.out.println(a);
+        System.out.println(b);
+    }
+}

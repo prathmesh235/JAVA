b4c64ccb00cfa9f6d75cb16319328c2889138fcb
Prathmesh Panmand <prathmeshpanmand23@gmail.com>
2026-07-12 16:44:54 +0530

    Arithmetic Modelo

---
+++ arithmetic_modelo.java
@@ -0,0 +1,11 @@
+public class arithmetic_modelo {
+    public static void main(String[] args) {
+        
+        int a = 84;
+        int b = 32;
+
+        int mod = a % b;
+
+        System.out.println(mod);
+    }
+}

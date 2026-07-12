e5f59661fd082ffba32c487f9e7a7abbbc31a95c
Prathmesh Panmand <prathmeshpanmand23@gmail.com>
2026-07-12 16:26:14 +0530

    Arithmetic Multiplication

---
+++ arithmetic_mul.java
@@ -0,0 +1,10 @@
+public class arithmetic_mul {
+    public static void main(String[] args) {
+        int a = 10;
+        int b = 2;
+
+        int mul = a*b;
+
+        System.out.println(mul);
+    }
+}

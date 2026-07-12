caec993860d540e58900cab50215b87b72e5e781
Prathmesh Panmand <prathmeshpanmand23@gmail.com>
2026-07-12 16:05:19 +0530

    Arithmetic substraction using user

---
+++ arithmetic_sub_user.java
@@ -0,0 +1,15 @@
+
+import java.util.Scanner;
+
+public class arithmetic_sub_user {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+         
+        int a=sc.nextInt();
+        int b=sc.nextInt();
+
+        int diff=a-b;
+
+        System.out.println(diff);
+    }
+}

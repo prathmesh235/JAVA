78e6d9c3bbd8ea7ee5ad10338a985a78c12fd094
Prathmesh Panmand <prathmeshpanmand23@gmail.com>
2026-07-12 16:40:37 +0530

    Arithmetic Division by user

---
+++ arithmetic_div_user.java
@@ -0,0 +1,15 @@
+
+import java.util.Scanner;
+
+public class arithmetic_div_user {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+        
+        int div=a/b;
+
+        System.out.println(div);
+    }
+}

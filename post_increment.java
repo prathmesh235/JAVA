6a33da121d8247c0a80cd7987c40f789e139a3bb
Prathmesh Panmand <prathmeshpanmand23@gmail.com>
2026-07-12 15:37:39 +0530

    Post increment

---
+++ post_increment.java
@@ -0,0 +1,10 @@
+public class post_increment {
+    public static void main(String[] args) {
+        int a=35;
+        int b=0;
+
+        b=a++;//a=36 b=35
+        System.out.println(a);
+        System.out.println(b);
+    }
+}

942d697fc1dbeb71adc0971dffb3f13364ac9b9b
Prathmesh Panmand <prathmeshpanmand23@gmail.com>
2026-07-12 16:00:59 +0530

    arithmetic Substraction

---
+++ arithmetic_sub.java
@@ -0,0 +1,10 @@
+public class arithmetic_sub {
+    public static void main(String[] args) {
+        int a=36;
+        float b=55;
+
+        float diff=a-b;
+
+        System.out.println(diff);
+    }
+}

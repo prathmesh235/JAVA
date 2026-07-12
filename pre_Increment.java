01263d696666395697d2d3565715b15a51a2940c
Prathmesh Panmand <prathmeshpanmand23@gmail.com>
2026-07-12 15:34:10 +0530

    Pre increment

---
+++ pre_Increment.java
@@ -0,0 +1,10 @@
+public class pre_Increment {
+    public static void main(String[] args) {
+        int a=35;
+        int b=0;
+
+        b=++a;//a=11  b=11
+        System.out.println(a);
+        System.out.println(b);
+    }
+}

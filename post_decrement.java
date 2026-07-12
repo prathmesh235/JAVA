108969c60c8ac810739ad9e279a18c5847820037
Prathmesh Panmand <prathmeshpanmand23@gmail.com>
2026-07-12 15:44:48 +0530

    Post decrement

---
+++ post_decrement.java
@@ -0,0 +1,10 @@
+public class post_decrement {
+    public static void main(String[] args) {
+        int a=20;
+        int b=0;
+
+        b=a--;//a=19 b=20
+        System.out.println(a);
+        System.out.println(b);
+    }
+}

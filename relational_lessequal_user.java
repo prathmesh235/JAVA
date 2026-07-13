3469b1e9417675e50347a9ff902f79fc91c9a8ef
Prathmesh Panmand <prathmeshpanmand23@gmail.com>
2026-07-13 15:31:28 +0530

    Relational <= by user

---
+++ relational_lessequal_user.java
@@ -0,0 +1,13 @@
+
+import java.util.Scanner;
+
+public class relational_lessequal_user {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+
+        System.out.println(a<=b);
+    }
+}

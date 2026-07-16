cca6b42485572f87b6b55f640230de6a5e33ce8a
Prathmesh Panmand <prathmeshpanmand23@gmail.com>
2026-07-16 21:41:58 +0530

    Linear Time Complexity. O(n)         }

---
+++ linear_timecomplx.java
@@ -0,0 +1,8 @@
+public class linear_timecomplx {
+    public static void main(String[] args) {
+        int a = 0;
+        for(int i=1; i<=n;i++){
+            a=a+1; //Linear Time Complexity. O(n)
+        }
+    }
+}

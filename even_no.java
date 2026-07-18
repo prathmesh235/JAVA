bd4b985c485beedb9d8020cc5b37c1b0d18f6ace
Prathmesh Panmand <prathmeshpanmand23@gmail.com>
2026-07-18 13:15:24 +0530

    Homework Que- Print all even numbers till n.

---
+++ even_no.java
@@ -0,0 +1,11 @@
+public class even_no {
+    public static void main(String[] args) {
+        int n=50;
+
+        for(int i=0;i<=n;i++){
+            if(i%2==0){
+                System.out.println(i);
+            }
+        }
+    }
+}

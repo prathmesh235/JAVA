cb797351a66081ce1861224a46e0d643805dc09a
Prathmesh Panmand <prathmeshpanmand23@gmail.com>
2026-07-19 20:31:27 +0530

    Logical Not(!)

---
+++ logical_not.java
@@ -0,0 +1,13 @@
+public class logical_not {
+    public static void main (String args[]){
+        int a=20;
+        int b=48;
+        //int c=49;
+
+        if(!(a>b)){
+        System.out.println("True");
+        }else{
+            System.out.println("False");
+        }
+    }
+}

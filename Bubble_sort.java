4bea5f9721f510abc7c4049bf4fcc598c3bd067b
Prathmesh Panmand <prathmeshpanmand23@gmail.com>
2026-07-20 18:46:52 +0530

    Bubble Sort Example

---
+++ Bubble_sort.java
@@ -0,0 +1,22 @@
+
+public class Bubble_sort {
+        public static void printArray(int arr[] ){
+            for(int i=0; i<arr.length;i++){
+                System.out.print(arr[i]+" ");
+            }
+        System.out.println();
+    }
+    public static void main(String[] args) {
+        int arr[] = {7,5,3,4,8,2,1};
+        for(int i=0;i<arr.length-1;i++){
+            for(int j=0;j<arr.length-i-1;j++){
+                if(arr[j]>arr[j+1]){
+                    int temp=arr[j];
+                    arr[j]=arr[j+1];
+                    arr[j+1]=temp;
+                }
+            }
+        }
+        printArray(arr);
+    }
+}

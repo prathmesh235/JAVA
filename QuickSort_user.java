7e466911f0f0c201fbd78eeadcb66a35b6ed93af
Prathmesh Panmand <prathmeshpanmand23@gmail.com>
2026-07-22 19:55:00 +0530

    Quick Sort by user

---
+++ QuickSort_user.java
@@ -0,0 +1,67 @@
+
+import java.util.Scanner;
+
+public class QuickSort_user {
+
+    public static int partition(int arr[], int low, int high) {
+        int pivot = arr[high];
+        int i = low - 1;
+
+        for (int j = low; j < high; j++) {
+            if (arr[j] < pivot) {
+                i++;
+
+                int temp = arr[i];
+                arr[i] = arr[j];
+                arr[j] = temp;
+            }
+        }
+
+        int temp = arr[i + 1];
+        arr[i + 1] = arr[high];
+        arr[high] = temp;
+
+        return i + 1;
+    }
+
+    public static void quickSort(int arr[], int low, int high) {
+        if (low < high) {
+            int pivotIndex = partition(arr, low, high);
+
+            quickSort(arr, low, pivotIndex - 1);
+            quickSort(arr, pivotIndex + 1, high);
+        }
+    }
+
+    public static void printArray(int arr[]) {
+        for (int i = 0; i < arr.length; i++) {
+            System.out.print(arr[i] + " ");
+        }
+        System.out.println();
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter size of array: ");
+        int n = sc.nextInt();
+
+        int arr[] = new int[n];
+
+        System.out.println("Enter array elements:");
+
+        for (int i = 0; i < n; i++) {
+            arr[i] = sc.nextInt();
+        }
+
+        System.out.println("Before Sorting:");
+        printArray(arr);
+
+        quickSort(arr, 0, n - 1);
+
+        System.out.println("After Sorting:");
+        printArray(arr);
+
+        sc.close();
+    }
+}

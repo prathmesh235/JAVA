bf0729567c053984f01086550ab34b1c64294708
Prathmesh Panmand <prathmeshpanmand23@gmail.com>
2026-07-27 14:53:03 +0530

    OOPS Parameterized Constructer Student Ex

---
+++ Con_Studd_Non_P.java
@@ -0,0 +1,33 @@
+
+
+class Studd{
+    String name;
+    int age;
+
+    public void printINfo(){
+        System.out.println(this.name);
+        System.out.println(this.age);
+
+    }
+
+    Studd(String name,int age){
+        this.name=name;
+        this.age=age;
+        
+    }
+}
+
+public class Con_Studd_Non_P{
+    public static void main(String[] args) {
+        
+        Studd s1=new Studd("Prathmesh",20);
+        
+        Studd s2=new Studd("Shraddha",19);
+        
+
+        s1.printINfo();
+        s2.printINfo();
+
+    }
+    
+}

c093f07c243f7cd2f33815186d3278d89659c9bb
Prathmesh Panmand <prathmeshpanmand23@gmail.com>
2026-07-27 17:36:58 +0530

    Inheritance Ex

---
+++ inheritance.java
@@ -0,0 +1,18 @@
+
+class Shape1{
+    String color;
+}
+    class Triangle extends Shape1{
+
+    }
+
+public class inheritance {
+    public static void main(String[] args) {
+        Triangle t1= new Triangle();
+        t1.color="Sky Blue";
+
+        System.out.println(t1.color);
+
+    }
+
+}

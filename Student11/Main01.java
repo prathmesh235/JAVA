19e70ca7efc16d7c876d229fe54454b23dc6c241
Prathmesh Panmand <prathmeshpanmand23@gmail.com>
2026-07-28 18:20:09 +0530

    Access modifiers-Private

---
+++ Student11/Main01.java
@@ -0,0 +1,9 @@
+package Student11;
+
+public class Main01 {
+    public static void main(String[] args) {
+        Student11 s =new Student11();
+        
+        s.Showage();
+    }
+}
---
+++ Student11/Student11.java
@@ -0,0 +1,9 @@
+package Student11;
+
+public class Student11 {
+    public int age=20;
+
+    public void Showage(){
+        System.out.println(age);
+    }
+}

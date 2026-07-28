2cd610ed1b9b58752adf997b6161c3580fe5c5af
Prathmesh Panmand <prathmeshpanmand23@gmail.com>
2026-07-28 17:29:01 +0530

    Move Oops and Bank into bank package

---
+++ bank/Oops.java
@@ -0,0 +1,10 @@
+package bank;
+public class Oops {
+    public static void main(String[] args) {
+        Bank account1 = new Bank();
+        account1.name = "Prathmesh";
+
+        System.out.println(account1.name);
+    }
+}
+

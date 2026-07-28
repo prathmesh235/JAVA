171283750710a32a27b52a214415b53ad40d7e62
Prathmesh Panmand <prathmeshpanmand23@gmail.com>
2026-07-28 18:31:36 +0530

    Access Modifier-Protected

---
+++ Student02/Main02.java
@@ -0,0 +1,5 @@
+package Student02;
+
+public class Main02 {
+    protected  String course="Bca";
+}
---
+++ Student02/Student02.java
@@ -0,0 +1,10 @@
+package Student02;
+
+public class Student02 {
+    public static void main(String[] args) {
+        Student02 s = new Student02();
+        // System.out.println(s.course);
+
+      
+    }
+}

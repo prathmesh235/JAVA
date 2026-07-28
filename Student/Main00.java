02fb2933bd2f48209224bdeea1be656b17b00db3
Prathmesh Panmand <prathmeshpanmand23@gmail.com>
2026-07-28 18:03:33 +0530

    Access modifiers-Public

---
+++ Student/Main00.java
@@ -0,0 +1,8 @@
+package Student;
+
+public class Main00 {
+    public static void main(String[] args) {
+        Student00 s = new Student00();
+        System.out.println(s.name);  
+    }
+}
---
+++ Student/Student00.java
@@ -0,0 +1,5 @@
+package Student;
+
+public class Student00 {
+    public String name = "Prathmesh";
+}

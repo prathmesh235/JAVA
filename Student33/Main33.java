56b48ef38ee772cbe9b11a5db64e181f59ea9ad6
Prathmesh Panmand <prathmeshpanmand23@gmail.com>
2026-07-28 18:48:54 +0530

    Access Modifier Default

---
+++ Student33/Main33.java
@@ -0,0 +1,8 @@
+package Student33;
+
+public class Main33 {
+    public static void main(String[] args) {
+        Student33 s = new Student33();
+        System.out.println(s.college33);
+    }
+}
---
+++ Student33/Student33.java
@@ -0,0 +1,6 @@
+
+package Student33;
+
+public class Student33 {
+    String college33 = "ABC College";
+}

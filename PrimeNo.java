534507a8aa224850d90526476f9e0749180326ca
Prathmesh Panmand <prathmeshpanmand23@gmail.com>
2026-08-06 08:51:13 +0530

    Prime No Ex

---
+++ PrimeNo.java
@@ -0,0 +1,32 @@
+
+import java.util.Scanner;
+
+public class PrimeNo {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter a number: ");
+        int n = sc.nextInt();
+
+        boolean prime = true;
+
+        if (n <= 1) {
+            prime = false;
+        } else {
+            for (int i = 2; i <= Math.sqrt(n); i++) {
+                if (n % i == 0) {
+                    prime = false;
+                    break;
+                }
+            }
+        }
+
+        if (prime) {
+            System.out.println("Prime Number");
+        } else {
+            System.out.println("Not a Prime Number");
+        }
+
+        sc.close();
+    }
+}

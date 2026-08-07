ce8108c0731ff228993c68421ab03c0dd00db789
Prathmesh Panmand <prathmeshpanmand23@gmail.com>
2026-08-07 12:31:21 +0530

    Finding a cube

---
+++ Cube_find.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+
+class Cube_find {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int n = sc.nextInt();
+
+        System.out.println("Cube = " + (n * n * n));
+    }
+}

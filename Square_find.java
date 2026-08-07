2f782e2fea4dfd828fdd2e80b4d946c8a76cf293
Prathmesh Panmand <prathmeshpanmand23@gmail.com>
2026-08-07 12:26:03 +0530

    Square_find

---
+++ Square_find.java
@@ -0,0 +1,12 @@
+
+import java.util.Scanner;
+
+class Square_find {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int n = sc.nextInt();
+
+        System.out.println("Square = " + (n * n));
+    }
+}
